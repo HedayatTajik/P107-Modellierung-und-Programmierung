cd9ca404d9efc8fd80575c02d9f4119a1a8ff51f
HedayatTajik <hedayatullah.tajik2@outlook.com>
2021-03-17 10:34:15 +0100

    add: Structorize + pap files2

--- src/app/zeichenkette/zeichenkette/zeichenkette.component.ts
+++ src/app/zeichenkette/zeichenkette/zeichenkette.component.ts
@@ -20,7 +20,7 @@ export class ZeichenketteComponent implements OnInit {
     private netWeightService: NetWeightService,
     private checkIfOnlyNummberService: CheckIfOnlyNummberService,
     private zeichenketteService: ZeichenketteService
-  ) {}
+  ) { }
 
   x = 3;
   y = 1;
@@ -43,14 +43,14 @@ export class ZeichenketteComponent implements OnInit {
     //   if(this.schreiben === 50)
     //   break
     // }
-    let summe = 0
-        for (let index = 100; index <= 1000; index++) {
-          summe = summe + index
-          console.log("index",index)
+    // let summe = 0
+    //     for (let index = 100; index <= 1000; index++) {
+    //       summe = summe + index
+    //       console.log("index",index)
 
+    //     }
+    //     console.log("summe",summe)
 
-        }
-        console.log("summe",summe)
 
     // let summe = 0;
     // for (let z = 5; z > 0; z--) {
@@ -58,6 +58,7 @@ export class ZeichenketteComponent implements OnInit {
     //   console.log('z', z);
     //   console.log('summe', summe);
     // }
+
 
     // let array: number[] = [10,3,5,9,2,8,2,4,8,7]
     // let max = array[0]
@@ -68,6 +69,78 @@ export class ZeichenketteComponent implements OnInit {
     //   }
     // }
     // console.log("zahl", max)
+
+
+
+    // let messwertArray = [24, 0, 13, 0, 47, 55, 0];
+    // let maxNummber = messwertArray[0]
+    // let averageNumber = 0
+    // let summeNumber = 0
+    // for (let index = 0; index < messwertArray.length; index++) {
+    //   summeNumber = summeNumber + messwertArray[index]
+    //   if (maxNummber < messwertArray[index]) {
+    //     maxNummber = messwertArray[index]
+    //   }
+    // }
+    // averageNumber = summeNumber / messwertArray.length
+
+    // console.log("maxNumb.", maxNummber)
+    // console.log("summeNumber.", summeNumber)
+    // console.log("averageNumber.", averageNumber)
+
+
+    // //Aufgabe 16
+    // let menge = 10
+    // let rabbat = 0
+    // if(menge >= 10 ){
+    //   rabbat = 5
+    //   if(menge >= 50){
+    //     rabbat = 7
+    //     if(menge >= 100){
+    //       rabbat = 10
+    //     }
+    //   }
+    // }
+    // console.log("menge",menge)
+    // console.log("rabbat",rabbat)
+
+
+    //Aufgabe 19
+
+    let maschNr = 0
+    let map = new Map<string, number>();
+    let obj = [
+      { "MaschNr": "100", "Stillstand": 1 },
+      { "MaschNr": "100", "Stillstand": 6 },
+      { "MaschNr": "101", "Stillstand": 2 },
+      { "MaschNr": "103", "Stillstand": 7 },
+      { "MaschNr": "103", "Stillstand": 12 },
+      { "MaschNr": "104", "Stillstand": 122 },
+    ]
+    obj.forEach(el => {
+      if (map.has(el.MaschNr)) {
+        maschNr = map.get(el.MaschNr) + el.Stillstand
+        map.set(el.MaschNr, maschNr)
+      }
+      else {
+        map.set(el.MaschNr, el.Stillstand)
+      }
+    })
+    console.log("map ", map)
+
+    map.forEach((value, key) => {
+      if (value > 5 && value < 29) {
+        console.log(key)
+      }
+      if (value > 30) {
+        key = key + "*"
+        console.log(key)
+      }
+
+    })
+
+
+
   }
 
   sendMethode(strichcodeString) {
